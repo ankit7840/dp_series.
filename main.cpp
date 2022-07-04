d161b5bb46e41f6029c597165d2a6ce17c5ab194
Ankit kumar <2020ugcs004@nitjsr.ac.in>
2022-07-05 00:26:51 +0530

    the triangle problem

--- main.cpp
+++ main.cpp
@@ -1,14 +1,10 @@
-// C++ program to find the
-// maximum number of bridges
-// in any path of the given graph
+
 
 
 #include <bits/stdc++.h>
 using namespace std;
 int main ()
-{  int t;
-     cin>>t;
-     cout<<t<<endl;
+{
 
 }
 
