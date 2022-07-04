f26c3db4501fbc6cb5a6aa61bc90eb013c5ccb70
Ankit kumar <2020ugcs004@nitjsr.ac.in>
2022-07-05 00:30:42 +0530

    the triangle problem

--- main.cpp
+++ main.cpp
@@ -5,7 +5,16 @@
 using namespace std;
 int main ()
 {
+    int a;
+    cin>>a;
+    int arr[a];
+     for ( int i=0;i<a;i++)
+     {
+         cin>>arr[i];
+     }
+     for( int i=0;i<a;i++){
 
+     }
 }
 
 
